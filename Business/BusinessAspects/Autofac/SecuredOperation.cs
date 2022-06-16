1d54591c82b33a029bca983de62a0a94c1a55925
MehmetUgur7 <77232030+MehmetUgur7@users.noreply.github.com>
2022-06-16 16:10:17 +0300

    16 june 5th commit

--- Business/BusinessAspects/Autofac/SecuredOperation.cs
+++ Business/BusinessAspects/Autofac/SecuredOperation.cs
@@ -1,15 +1,13 @@
-﻿using Business.Constants;
-using Castle.DynamicProxy;
-using Core.Utilities.Interceptors;
+﻿using Core.Utilities.Interceptors;
 using Core.Utilities.IoC;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Core.Extensions;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Castle.DynamicProxy;
+using Microsoft.Extensions.DependencyInjection;
+using Core.Extensions;
+using Business.Constants;
 
 namespace Business.BusinessAspects.Autofac
 {
